ae14623057443b8da82bb489fa4f7c95543ad798
HeYiding <yiding.he@gmail.com>
2017-09-21 00:28:12 +0800

    ...

--- src/main/java/com/hyd/steamgss/Fx.java
+++ src/main/java/com/hyd/steamgss/Fx.java
@@ -2,6 +2,9 @@ package com.hyd.steamgss;
 
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
 
 /**
  * @author yidin
@@ -10,5 +13,15 @@ public class Fx {
 
     public static Button button(String text) {
         return new Button(text);
+    }
+
+    public static Label label(String text) {
+        Label label = new Label(text);
+        label.setMinWidth(Region.USE_PREF_SIZE);
+        return label;
+    }
+
+    public static TextField textField() {
+        return new TextField();
     }
 }
--- src/main/java/com/hyd/steamgss/ui/GameConfigurationPane.java
+++ src/main/java/com/hyd/steamgss/ui/GameConfigurationPane.java
@@ -1,10 +1,50 @@
 package com.hyd.steamgss.ui;
 
-import javafx.scene.layout.VBox;
+import com.hyd.steamgss.Fx;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
 /**
  * @author yidin
  */
-public class GameConfigurationPane extends VBox {
+public class GameConfigurationPane extends GridPane {
 
+    private final TextField txtGameName = Fx.textField();
+
+    public GameConfigurationPane() {
+        this.setHgap(10);
+        this.setVgap(10);
+        this.setPadding(new Insets(10));
+
+        this.getColumnConstraints().addAll(
+                headerCons(),
+                fieldCons()
+        );
+
+        ///////////////////////////////////////////////
+
+        this.add(Fx.label("游戏名称："), 0, 0);
+        this.add(txtGameName, 1, 0);
+
+        ///////////////////////////////////////////////
+
+        GridPane.setHgrow(txtGameName, Priority.ALWAYS);
+    }
+
+    private ColumnConstraints fieldCons() {
+        ColumnConstraints cc = new ColumnConstraints();
+        cc.setPrefWidth(100);
+        cc.setHalignment(HPos.LEFT);
+        cc.setFillWidth(true);
+        cc.setHgrow(Priority.ALWAYS);
+        return cc;
+    }
+
+    private ColumnConstraints headerCons() {
+        return new ColumnConstraints();
+    }
 }
